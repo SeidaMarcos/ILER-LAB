92d4ce256229966c810021294f609010d8d0fde2
Marcos Robador Mateos <marcoscabet@gmail.com>
2024-11-26 12:48:03 +0100

    migration tasks

---
+++ database/migrations/2024_11_26_114635_create_tasks_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->enum('priority', ['baja', 'media', 'alta', 'urgente']);
+            $table->enum('progress', ['0', '25', '50', '75', '100']);
+            $table->date('date');
+            $table->string('pdf')->nullable(); // Path del archivo PDF
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
